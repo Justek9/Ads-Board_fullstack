559a6e8c192ea865f40f354c84a47dbc197493a8
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-11-09 09:49:52 +0100

    connect to db, add midlware in server file

---
+++ db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const connectToDB = () => {
+	const password = process.env.DB_PASSWORD
+	const uri = `mongodb+srv://zagorskaj:${password}@cluster0.gh0ncql.mongodb.net/ADSboard?retryWrites=true&w=majority`
+
+	// connect to DB
+	mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+	const db = mongoose.connection
+
+	// on success
+	db.once('open', () => {
+		console.log('Connected to the database')
+	})
+
+	// on error
+	db.on('error', err => console.log('Error ' + err))
+}
+
+module.exports = connectToDB
--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const connectToDB = require('./db')
+const dotenv = require('dotenv').config()
 
+// start express server
 const app = express()
+const server = app.listen(process.env.PORT || 8000, () => {
+	console.log('Server is running...')
+})
 
-app.listen(8000, () => {
-	console.log('Server is running on port: 8000')
+// connect to DB
+connectToDB()
+
+// add midllwares
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use((req, res) => {
+	res.status(404).send({ message: 'Not found...' })
 })
