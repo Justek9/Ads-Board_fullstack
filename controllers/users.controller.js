721cb19d963e7e7b2774f40e956d65742f79bf79
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-11-09 16:04:19 +0100

    add auth middlware to check if user is logged

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,11 +3,12 @@ const Users = require('../models/Users.model')
 // get all logged users
 exports.getLoggedUser = async (req, res) => {
 	try {
-		const user = await Users.find()
-		res.json(users)
+		// const users = await Users.find()
+		// res.json(users)
 	} catch (err) {
 		res.status(500).json({ message: err.message })
 	}
 }
+
 
 
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const authMiddlware = require('../utils/authMiddlware')
 
 const users = require('../controllers/users.controller')
 
-router.get('/user', users.getLoggedUser)
+router.get('/user', authMiddlware, users.getLoggedUser)
 
 module.exports = router
