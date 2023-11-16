3fa1e59a8f80749a1ab4889a26385fe9c1ba0921
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-11-16 13:16:33 +0100

    style single ad

--- client/src/components/features/SingleAd.js
+++ client/src/components/features/SingleAd.js
@@ -6,23 +6,33 @@ import Card from 'react-bootstrap/Card'
 import { IMGS_URL } from '../../config'
 import { useState } from 'react'
 import DeleteModal from './DeleteModal'
+import styles from './SingleAd.module.scss'
 
 const SingleAd = () => {
 	const { id } = useParams()
 	const ad = useSelector(state => getAdbyId(state, id))[0]
 	const [modalShow, setModalShow] = useState(false)
-	
+
+	console.log(ad.user)
+
 	return (
-		<Card>
-			<Card.Img variant='top' src={IMGS_URL + ad.src} />
+		<Card className={styles.card}>
+			<Card.Img variant='top' src={IMGS_URL + ad.src} className={styles.img} />
 			<Card.Body>
 				<Card.Title>{ad.title}</Card.Title>
-				<Card.Text>{ad.location}</Card.Text>
-
-				<Button variant='primary'>Edit</Button>
-				<Button variant='danger' onClick={() => setModalShow(true)}>
-					Delete
-				</Button>
+				<Card.Text>{ad.text}</Card.Text>
+				<Card.Text>Price: ${ad.price}</Card.Text>
+				<Card.Text>Seller: {ad.user}</Card.Text>
+				<Card.Text>Location: {ad.location}</Card.Text>
+				<Card.Text>Published date: {ad.date}</Card.Text>
+				<div className={styles.btnContaier}>
+					<Button className={styles.btn} variant='primary'>
+						Edit
+					</Button>
+					<Button className={styles.btn} variant='danger' onClick={() => setModalShow(true)}>
+						Delete
+					</Button>
+				</div>
 			</Card.Body>
 			<DeleteModal id={ad._id} show={modalShow} onHide={() => setModalShow(false)} />
 		</Card>
